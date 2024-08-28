1eeb0c31b05a4795083d39ebad93dc85bd25f517
Ulil Albab <ulil.alb21@gmail.com>
2024-08-28 22:24:34 +0700

    font and layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      white: "hsl(0, 0 %, 100 %)",
+      blue: {
+        marine: "hsl(213, 96%, 18%)",
+        purplish: "hsl(243, 100%, 62%)",
+        pastel: "hsl(228, 100%, 84%)",
+        light: "hsl(206, 94%, 87%)",
+      },
+      red: {
+        strawberry: "hsl(354, 84%, 57%)",
+      },
+      gray: {
+        cool: "hsl(231, 11%, 63%)",
+        light: "hsl(229, 24%, 87%)",
+      },
+      magnolia: "hsl(217, 100%, 97%)",
+      alabaster: "hsl(231, 100%, 99%)",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
